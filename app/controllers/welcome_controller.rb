1514d0cb90e909fcae89c892a262e3e86d6b059b
Francieli <francielifrv@gmail.com>
2013-10-02 22:19:00 -0300

    Configurando layout com partial e content_for

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
-	def index
+	#layout Applicatioon_black
 
+	def index
+		@name = 'Francieli'
 	end
 	def black 
 		render :layout => "application_black" 
